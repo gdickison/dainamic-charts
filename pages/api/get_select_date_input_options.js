62645014b73905b08acc2e32886b175fb6e32ad3
paunchymonk <gregory@dickison.net>
2022-06-15 12:33:30 -0700

    Remove commented lines from API files

--- pages/api/get_select_date_input_options.js
+++ pages/api/get_select_date_input_options.js
@@ -8,7 +8,6 @@ export default async function handler(req, res) {
     .query(`SELECT date
       FROM banking_app.available_dates
       ORDER BY date;`)
-    // .then(response => console.log(response))
     .then(response => res.status(200).json({response: response.rows}))
     .then(client.release())
     .catch(error => console.log("There is an error getting top feature data: ", error))
--- pages/api/get_select_msa_input_options.js
+++ pages/api/get_select_msa_input_options.js
@@ -8,7 +8,6 @@ export default async function handler(req, res) {
     .query(`SELECT msa_code, msa_name
       FROM banking_app.msa_names
       ORDER BY msa_code;`)
-    // .then(response => console.log(response))
     .then(response => res.status(200).json({response: response.rows}))
     .then(client.release())
     .catch(error => console.log("There is an error getting top feature data: ", error))
