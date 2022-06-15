f7929715d18fe6dd7b8233f89738e4a9da30ccd8
paunchymonk <gregory@dickison.net>
2022-06-15 14:57:00 -0700

    Add a loading state to the form inputs so they don't show up until the options are loaded - prevents an ugly resize of the inputs

--- pages/index.js
+++ pages/index.js
@@ -27,12 +27,15 @@ ChartJS.register(
   Legend
 )
 
+import Loader from "../src/components/Loader"
+
 import { Bar } from "react-chartjs-2"
 
 import TempLogin from "../src/components/TempLogin"
 import TopFeatures from "../src/components/TopFeatures"
 
 const Home = () => {
+  const [isLoading, setLoading] = useState(false)
   const [isLoggedIn, setLoggedIn] = useState(false)
   const [msaOptions, setMsaOptions] = useState()
   const [monthOptions, setMonthOptions] = useState()
@@ -99,8 +102,10 @@ const Home = () => {
   }
 
   useEffect(() => {
+    setLoading(true)
     getSelectMsaInputOptions()
     getSelectDateInputOptions()
+    setLoading(false)
   },[])
 
   const handleDateChange = e => {
@@ -463,6 +468,10 @@ const Home = () => {
       setShowTopFeatures(true)
   }
 
+  if(isLoading) {
+    return <Loader/>
+  }
+
   return (
     <>
       <Head>
@@ -474,90 +483,92 @@ const Home = () => {
         </h1>
       </header>
       {isLoggedIn ? <main className="h-screen">
-        <section className="flex flex-col items-center justify-center px-20">
-          <form className="flex flex-col items-center" action="#">
-            <div className="flex flex-col md:flex-row items-center">
-              <div className="flex flex-col md:space-y-4">
-                <label className="text-2xl mx-2" htmlFor="startDate">Select a start date: </label>
-                <select className="mx-2 w-max text-center md:text-left md:px-2 border-2 border-blue-400 bg-white rounded-md text-xl" id="startDate" name="startDate" defaultValue="" onChange={handleDateChange}>
-                  <option disabled></option>
-                  {monthOptions && monthOptions.map(month => {
-                    return (
-                      <option key={month} value={month}>{month}</option>
-                    )
-                  })}
-                </select>
-              </div>
-              <div className="flex flex-col md:space-y-4">
-                <label className="text-2xl mx-2"  htmlFor="endDate">Select an end date: </label>
-                <select className="mx-2 w-max text-center md:text-left md:px-2 border-2 border-blue-400 bg-white rounded-md text-xl" id="endDate" name="endDate" defaultValue="" onChange={handleDateChange}>
-                  <option disabled></option>
-                  {monthOptions && monthOptions.map(month => {
-                    return (
-                      <option key={month} value={month}>{month}</option>
-                    )
-                  })}
-                </select>
-              </div>
-  {/* Select Target Region */}
-              <div className="flex flex-col md:space-y-4">
-                <label className="text-2xl mx-2" htmlFor="targetMsaCode">Select a Target Region: </label>
-                <select className="mx-2 w-80 md:w-max text-center md:text-left md:px-2 border-2 border-blue-400 bg-white rounded-md text-xl" id="targetMsaCode" name="targetMsaCode" defaultValue="" onChange={handleTargetRegionChange}>
-                  <option disabled></option>
-                  {msaOptions && msaOptions.map(singleMsa => {
-                    return (
-                      <option key={singleMsa.msa_code} value={singleMsa.msa_code} data-display={`${singleMsa.msa_name} (${singleMsa.msa_code})`}>{singleMsa.msa_name} ({singleMsa.msa_code})</option>
-                    )
-                  })}
-                </select>
-              </div>
-            </div>
-{/* Select & display comparison regions */}
-            {targetRegion &&
-              <div className="w-full mt-4 flex flex-col space-y-2">
-                <div>
-                  <div className="flex flex-col md:space-y-4 float-right">
-                    <label className="text-2xl mx-2" htmlFor="compMsaCode">Select Up to Two Comparison Regions (optional): </label>
-                    <select className="mx-2 w-80 md:w-max text-center md:text-left md:px-2 border-2 border-blue-400 bg-white rounded-md text-xl" id="compMsaCode" name="compMsaCode" defaultValue="" onChange={handleCompRegionChange}>
-                      <option disabled></option>
-                      {msaOptions && msaOptions.map(singleMsa => {
-                        return (
-                          <option key={singleMsa.msa_code} value={singleMsa.msa_code} data-display={`${singleMsa.msa_name} (${singleMsa.msa_code})`}>{singleMsa.msa_name} ({singleMsa.msa_code})</option>
-                        )
-                      })}
-                    </select>
-                  </div>
+        {msaOptions && monthOptions &&
+          <section className="flex flex-col items-center justify-center px-20">
+            <form className="flex flex-col items-center" action="#">
+              <div className="flex flex-col md:flex-row items-center">
+                <div className="flex flex-col md:space-y-4">
+                  <label className="text-2xl mx-2" htmlFor="startDate">Select a start date: </label>
+                  <select className="mx-2 w-max text-center md:text-left md:px-2 border-2 border-blue-400 bg-white rounded-md text-xl" id="startDate" name="startDate" defaultValue="" onChange={handleDateChange}>
+                    <option disabled></option>
+                    {monthOptions && monthOptions.map(month => {
+                      return (
+                        <option key={month} value={month}>{month}</option>
+                      )
+                    })}
+                  </select>
                 </div>
-                <div>
-                  <div className="w-1/2 float-right">
-                    <div className="text-2xl mx-2">Comparison Regions</div>
-                    <div className="min-h-[44px] m-2 text-center md:text-left border-2 border-blue-400 p-1 bg-white rounded-md text-xl">
-                      {compRegions.length > 0 &&
-                        compRegions.map(region => {
+                <div className="flex flex-col md:space-y-4">
+                  <label className="text-2xl mx-2"  htmlFor="endDate">Select an end date: </label>
+                  <select className="mx-2 w-max text-center md:text-left md:px-2 border-2 border-blue-400 bg-white rounded-md text-xl" id="endDate" name="endDate" defaultValue="" onChange={handleDateChange}>
+                    <option disabled></option>
+                    {monthOptions && monthOptions.map(month => {
+                      return (
+                        <option key={month} value={month}>{month}</option>
+                      )
+                    })}
+                  </select>
+                </div>
+    {/* Select Target Region */}
+                <div className="flex flex-col md:space-y-4">
+                  <label className="text-2xl mx-2" htmlFor="targetMsaCode">Select a Target Region: </label>
+                  <select className="mx-2 w-80 md:w-max text-center md:text-left md:px-2 border-2 border-blue-400 bg-white rounded-md text-xl" id="targetMsaCode" name="targetMsaCode" defaultValue="" onChange={handleTargetRegionChange}>
+                    <option disabled></option>
+                    {msaOptions && msaOptions.map(singleMsa => {
+                      return (
+                        <option key={singleMsa.msa_code} value={singleMsa.msa_code} data-display={`${singleMsa.msa_name} (${singleMsa.msa_code})`}>{singleMsa.msa_name} ({singleMsa.msa_code})</option>
+                      )
+                    })}
+                  </select>
+                </div>
+              </div>
+  {/* Select & display comparison regions */}
+              {targetRegion &&
+                <div className="w-full mt-4 flex flex-col space-y-2">
+                  <div>
+                    <div className="flex flex-col md:space-y-4 float-right">
+                      <label className="text-2xl mx-2" htmlFor="compMsaCode">Select Up to Two Comparison Regions (optional): </label>
+                      <select className="mx-2 w-80 md:w-max text-center md:text-left md:px-2 border-2 border-blue-400 bg-white rounded-md text-xl" id="compMsaCode" name="compMsaCode" defaultValue="" onChange={handleCompRegionChange}>
+                        <option disabled></option>
+                        {msaOptions && msaOptions.map(singleMsa => {
                           return (
-                            <p key={region.compMsaCode} className="inline-block w-max text-center md:text-left m-1 border-2 border-gray-300 md:px-2 bg-gray-200 text-xl leading-8 space-x-4">
-                              <span>{region.displayText}</span>
-                              <span className=" h-6 w-6 group hover:cursor-pointer" onClick={removeCompRegion}>
-                                <img id={region.compMsaCode} className="h-6 inline align-text-top" src="/close.svg" alt="remove region" />
-                              </span>
-                            </p>
+                            <option key={singleMsa.msa_code} value={singleMsa.msa_code} data-display={`${singleMsa.msa_name} (${singleMsa.msa_code})`}>{singleMsa.msa_name} ({singleMsa.msa_code})</option>
                           )
-                        })
-                      }
+                        })}
+                      </select>
+                    </div>
+                  </div>
+                  <div>
+                    <div className="w-1/2 float-right">
+                      <div className="text-2xl mx-2">Comparison Regions</div>
+                      <div className="min-h-[44px] m-2 text-center md:text-left border-2 border-blue-400 p-1 bg-white rounded-md text-xl">
+                        {compRegions.length > 0 &&
+                          compRegions.map(region => {
+                            return (
+                              <p key={region.compMsaCode} className="inline-block w-max text-center md:text-left m-1 border-2 border-gray-300 md:px-2 bg-gray-200 text-xl leading-8 space-x-4">
+                                <span>{region.displayText}</span>
+                                <span className=" h-6 w-6 group hover:cursor-pointer" onClick={removeCompRegion}>
+                                  <img id={region.compMsaCode} className="h-6 inline align-text-top" src="/close.svg" alt="remove region" />
+                                </span>
+                              </p>
+                            )
+                          })
+                        }
+                      </div>
                     </div>
                   </div>
                 </div>
-              </div>
-            }
-          </form>
+              }
+            </form>
 
-          {(!dateRange.startDate || !dateRange.endDate || !targetRegion) &&
-            <div className="flex items-center justify-center h-12 my-8 text-2xl">Select a start date, end date, and MSA to see results</div>
-          }
-          {dateRange.startDate && dateRange.endDate && targetRegion &&
-            <button className="flex items-center justify-center w-40 h-12 my-8 rounded-md p-4 bg-blue-400 hover:bg-blue-600 text-gray-100" onClick={getData}>See Results</button>
-          }
-        </section>
+            {(!dateRange.startDate || !dateRange.endDate || !targetRegion) &&
+              <div className="flex items-center justify-center h-12 my-8 text-2xl">Select a start date, end date, and MSA to see results</div>
+            }
+            {dateRange.startDate && dateRange.endDate && targetRegion &&
+              <button className="flex items-center justify-center w-40 h-12 my-8 rounded-md p-4 bg-blue-400 hover:bg-blue-600 text-gray-100" onClick={getData}>See Results</button>
+            }
+          </section>
+        }
         {targetRegionData &&
           <section className="mb-10">
             <header className="text-center my-10">
