c41981d2726e226d4984eb86ad2870c50901c81d
paunchymonk <gregory@dickison.net>
2022-07-02 10:14:24 -0700

    Add a table to the delinquency by number of borrowers chart to help explain the data, do some styling

--- src/components/DelinquencyByNumberOfBorrowers.js
+++ src/components/DelinquencyByNumberOfBorrowers.js
@@ -16,7 +16,7 @@ ChartJS.register(
 )
 
 import Loader from "./Loader"
-import { Bar, Pie } from "react-chartjs-2"
+import { Scatter, Bar, Pie } from "react-chartjs-2"
 import { useState, useEffect } from "react"
 import ChartHeaderWithTooltip from "./ChartHeaderWithTooltip"
 
@@ -96,23 +96,27 @@ const DelinquencyByNumberOfBorrowers = ({dateRange, targetRegion, compRegions}) 
           labels: barLabels,
           datasets: [
             {
+              type: "bar",
               label: "1 Borrower",
               backgroundColor: ["rgba(51, 177, 255, 0.5)"],
               borderColor: ["rgba(51, 177, 255, 1)"],
               hoverBackgroundColor: ["rgba(51, 177, 255, 1)"],
               borderWidth: 3,
               data: barDataOneBorrower,
-              tooltip: barTooltipOneBorrower
+              tooltip: barTooltipOneBorrower,
+              order: 1
             },
             {
+              type: "bar",
               label: "2+ Borrowers",
               backgroundColor: ["rgba(0, 83, 255, 0.5)"],
               borderColor: ["rgba(0, 83, 255, 1)"],
               hoverBackgroundColor: ["rgba(0, 83, 255, 1)"],
               borderWidth: 3,
               data: barDataMultiBorrower,
-              tooltip: barTooltipMultiBorrower
-            },
+              tooltip: barTooltipMultiBorrower,
+              order: 2
+            }
           ]
         }
 
@@ -120,6 +124,7 @@ const DelinquencyByNumberOfBorrowers = ({dateRange, targetRegion, compRegions}) 
           showLabel: false,
           responsive: true,
           aspectRatio: 1.75,
+          maxBarThickness: 125,
           plugins: {
             title: {
               text: "Delinquency Rate per Category",
@@ -131,7 +136,12 @@ const DelinquencyByNumberOfBorrowers = ({dateRange, targetRegion, compRegions}) 
             tooltip: {
               callbacks: {
                 beforeTitle: function(context){
-                  return `${context[0].label}`
+                  console.log('context', context)
+                  return [
+                    `${context[0].label}`,
+                    `Regional Delinquency Rate: ${context[0].dataset.tooltip[context[0].dataIndex].region_delinquency_rate}%`,
+                    ''
+                  ]
                 },
                 title: function(context){
                   return `${context[0].dataset.label}`
@@ -259,7 +269,7 @@ const DelinquencyByNumberOfBorrowers = ({dateRange, targetRegion, compRegions}) 
                   ]
                 },
                 label: function(context){
-                  return `Percentage of all delinquent loans: ${context.raw}%`
+                  return `Share of Regional Delinquency Rate: ${context.raw}%`
                 }
               }
             }
@@ -284,25 +294,45 @@ const DelinquencyByNumberOfBorrowers = ({dateRange, targetRegion, compRegions}) 
       <ChartHeaderWithTooltip
         chartName={"Delinquency By Number of Borrowers"}
         msa={compRegions.length > 0 ? "selected regions" : targetRegion.msaName}
-        tooltip={"All loans for each month are grouped by number of borrowers (1 or 2+). The number of loans with 3 or more borrowers are statistically insignificant and are included in the '2+' category."}
+        tooltip={"All loans for each month are grouped by number of borrowers (1 or 2+). The number of loans with 3 or more borrowers are statistically insignificant and are included in the '2+' category. Click on the legend to show/hide datasets"}
       />
-      <div className="flex flex-col items-center space-y-8">
-        {barChartData &&
-          <div className="relative flex w-1/2">
-            <Bar data={barChartData} options={barChartOptions} />
-          </div>
-        }
-        {pieChartData &&
-          <div className="flex">
-            {pieChartData.map((chart, i) => {
-              return (
-                <div className="relative flex">
-                  <Pie data={chart} options={pieChartOptions} />
-                </div>
-              )
-            })}
-          </div>
-        }
+      <div className="flex justify-around w-full">
+        <div className="w-1/3 justify-evenly flex flex-col">
+{console.log('barChartData', barChartData)}
+{console.log('pieChartData', pieChartData)}
+          {barChartData && barChartData.labels.map((label, i) => {
+            return (
+              <div className="flex flex-col">
+                <p key={i} className="pl-3 py-2 text-xl">{label} Region</p>
+                <ul className="pl-5 text-sm">
+                  <li>Regional Delinquency Rate: {barChartData.datasets[0].tooltip[i].region_delinquency_rate}%</li>
+                  <li>Delinquency Rate for loans with 1 borrower: {barChartData.datasets[0].tooltip[i].borrower_delinquency_rate}%</li>
+                  <li>Share of the Regional Delinquency Rate for 1 Borrower loans: {(Number(pieChartData[i].datasets[0].tooltip.oneBorrowerDelinquent) / Number(pieChartData[i].datasets[0].tooltip.totalDelinquent) * 100).toFixed(2)}%</li>
+                  <li>Delinquency Rate for loans with 2+ borrowers: {barChartData.datasets[1].tooltip[i].borrower_delinquency_rate}%</li>
+                  <li>Share of the Regional Delinquency Rate for 2+ Borrower loans: {(Number(pieChartData[i].datasets[0].tooltip.multiBorrowerDelinquent) / Number(pieChartData[i].datasets[0].tooltip.totalDelinquent) * 100).toFixed(2)}%</li>
+                </ul>
+              </div>
+            )
+          })}
+        </div>
+        <div className="flex flex-col items-center space-y-8 px-12 py-4 w-7/12">
+          {barChartData &&
+            <div className="flex w-11/12">
+              <Bar data={barChartData} options={barChartOptions} />
+            </div>
+          }
+          {pieChartData &&
+            <div className="flex">
+              {pieChartData.map((chart, i) => {
+                return (
+                  <div key={i} className="flex">
+                    <Pie data={chart} options={pieChartOptions} width={pieChartData.length === 1 ? 250 : 200} />
+                  </div>
+                )
+              })}
+            </div>
+          }
+        </div>
       </div>
     </div>
   )
