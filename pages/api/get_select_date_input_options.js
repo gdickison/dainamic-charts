99d6d34f4127e6439505ac168a950a0a57e2bf77
paunchymonk <gregory@dickison.net>
2022-08-09 13:29:22 -0700

    Troubleshooting 500 error

--- pages/api/get_select_date_input_options.js
+++ pages/api/get_select_date_input_options.js
@@ -1,14 +1,17 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import pool from '../../src/client'
+// // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+// import pool from '../../src/client'
 
-export default async function handler(req, res) {
-  const client = await pool.connect()
+// export default async function handler(req, res) {
+//   const client = await pool.connect()
 
-  await client
-    .query(`SELECT date
-      FROM banking_app.available_dates
-      ORDER BY date;`)
-    .then(response => res.status(200).json({response: response.rows}))
-    .then(client.release())
-    .catch(error => console.log("There is an error getting top feature data: ", error))
+//   await client
+//     .query(`SELECT date
+//       FROM banking_app.available_dates
+//       ORDER BY date;`)
+//     .then(response => res.status(200).json({response: response.rows}))
+//     .then(client.release())
+//     .catch(error => console.log("There is an error getting top feature data: ", error))
+// }
+export default function handler(req, res) {
+  res.status(200).json({ name: 'this is a date input test' })
 }
--- pages/api/get_select_msa_input_options.js
+++ pages/api/get_select_msa_input_options.js
@@ -1,14 +1,18 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import pool from '../../src/client'
+// import pool from '../../src/client'
 
-export default async function handler(req, res) {
-  const client = await pool.connect()
+// export default async function handler(req, res) {
+//   const client = await pool.connect()
 
-  await client
-    .query(`SELECT msa_code, msa_name
-      FROM banking_app.msa_names
-      ORDER BY msa_code;`)
-    .then(response => res.status(200).json({response: response.rows}))
-    .then(client.release())
-    .catch(error => console.log("There is an error getting top feature data: ", error))
+//   await client
+//     .query(`SELECT msa_code, msa_name
+//       FROM banking_app.msa_names
+//       ORDER BY msa_code;`)
+//     .then(response => res.status(200).json({response: response.rows}))
+//     .then(client.release())
+//     .catch(error => console.log("There is an error getting top feature data: ", error))
+// }
+
+export default function handler(req, res) {
+  res.status(200).json({ name: 'this is an msa input test' })
 }
